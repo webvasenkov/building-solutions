aecf90114b77e8560f450724fcfa16aba758eb49
Denis Vasenkov <webvasenkov@gmail.com>
2024-10-09 22:25:56 +0300

    add(optimization): accessibility & performance

--- src/components/home-sections/map.tsx
+++ src/components/home-sections/map.tsx
@@ -11,6 +11,7 @@ function Map() {
       className='relative mt-[72px] sm:mt-[140px] container'>
       <iframe
         className='w-full h-[230px] sm:h-[480px] map'
+        title='яндекс карта'
         src='https://yandex.ru/map-widget/v1/?um=constructor%3A53fa520d9d5e15f171be1ba53f58e6247807c648e5acc576bb658663ec894f00&amp;source=constructor'></iframe>
     </m.section>
   );
